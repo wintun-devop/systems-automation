68dca101eb16631f66ad248d1956ae48073ae8ef
Win Tun <wintun.hlaing@outlook.com>
2022-02-18 21:54:46 +0630

    v1.001

---
+++ nfs-server/nsf-ubuntu20.04lts/nfs-ubuntu20.04lts-server.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Win Tun Hlaing(https://www.youtube.com/channel/UCz9ebjc-_3t3p49gGpwyAKA/videos)
+
+#http://www.mmuak.net or https://mmuak.blogspot.com/
+
+# Update/Download package information from all configured sources
+apt update -y
+
+#Upgrading the downloaded packages
+apt upgrade -y
+
+#installing necessary packages and 
+apt install -y wget curl make unzip network-manager gcc net-tools nfs-kernel-server
+
+systemctl enable --now nfs-server
