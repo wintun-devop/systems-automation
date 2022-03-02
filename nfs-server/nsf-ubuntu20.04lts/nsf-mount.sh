7875354049f54b95e1daecd12f534ac0c422d2c5
Win Tun <wintun.hlaing@outlook.com>
2022-03-02 20:59:36 +0630

    v1.002

---
+++ nfs-server/nsf-ubuntu20.04lts/nsf-mount.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#https://github.com/wintun-devop(My github repo)
+
+#https://www.youtube.com/channel/UCz9ebjc-_3t3p49gGpwyAKA (Please subscribe my channel.Thank You!)
+
+#Assign correct nfs mount target id(Please make sure to correct mount target id)
+nsf_server_share_point="10.100.0.240:/mnt/dataf"
+
+#Define mount point directory as your desired
+nsf_client_mount_point=/mnt/nsf-mount-data
+
+#create mount point directory
+mkdir -p "${nsf_client_mount_point}"
+
+#mounting nsf on your desired mount point
+mount -vvv -t nfs "${nsf_server_share_point}" "${nsf_client_mount_point}"
+
+#checking the nfs mounting status
+mount | grep -i nfs
