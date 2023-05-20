817270db81b1e09922c95bdbae5936203d982400
Win Tun <wintun.hlaing@outlook.com>
2023-05-20 19:22:57 +0630

    v1.002

---
+++ nodejs/nodejs18xlts-ubuntu20.04lts.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#https://www.youtube.com/channel/UCz9ebjc-_3t3p49gGpwyAKA (Please subscribe my channel.Thank You!)
+
+#http://www.mmuak.net or https://mmuak.blogspot.com/
+
+# Update/Download package information from all configured sources
+apt update -y
+
+#Upgrading the downloaded packages
+apt upgrade -y
+
+#installing necessary modules before node-js
+apt install -y wget curl make unzip network-manager gcc net-tools lsb-release ca-certificates apt-transport-https gnupg software-properties-common
+
+#fatching repo for nodejs version 16.xlts
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+
+#installing nodejs
+apt-get install -y nodejs
+
+#check nodejs version
+node --version
