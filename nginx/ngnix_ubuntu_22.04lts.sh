b2771968f51e2b7763b824a005c5351af01e88e1
wintun-devop <wintun.hlaing@outlook.com>
2023-07-30 20:18:17 +0630

    v1.001

---
+++ nginx/ngnix_ubuntu_22.04lts.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#Update/Download package information from all configured sources
+sudo apt update -y
+
+#Upgrading the downloaded packages
+sudo apt upgrade -y
+
+#install necessary packages for prerequireties
+sudo apt install -y wget curl make unzip network-manager gcc net-tools lsb-release ca-certificates apt-transport-https gnupg2 software-properties-common
+
+#install ngnix
+sudo apt install nginx -y
+
+sudo systemctl start nginx
+
+sudo systemctl enable nginx
+
+sudo ufw allow 80/tcp
+
+sudo ufw allow 443/tcp
