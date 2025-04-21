d76d0074c058505c8f5584857f7f1b719cf75feb
Win Tun Hlaing <71956577+wintun-devop@users.noreply.github.com>
2025-04-21 18:24:06 +0630

    Update node-nvm.sh

--- nodejs/node-nvm.sh
+++ nodejs/node-nvm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm ls-remote
 nvm install --lts
