00153044c538cf89e213625d11c105fd589ef058
Win Tun Hlaing <71956577+wintun-devop@users.noreply.github.com>
2025-06-03 14:56:41 +0630

    Add files via upload

---
+++ dotnet/alma_linux9_dotnet8.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#necessary core system update
+dnf update -y
+
+#installing libreswan and necessary software packages 
+dnf install -y net-tools git curl wget unzip
+
+#install dotnet8
+dnf install -y dotnet-sdk-8.0
+
