97b76d4f37dbf89e1defab032dd70a706a0d37b8
Win Tun Hlaing <71956577+wintun-devop@users.noreply.github.com>
2025-07-15 13:45:49 +0630

    Update mysql84_redhat9.sh

--- mysql8.0/mysql84_redhat9.sh
+++ mysql8.0/mysql84_redhat9.sh
@@ -12,11 +12,11 @@ dnf install -y gcc net-tools curl wget unzip
 echo "Necessary software packages installation done!"
 
 #Downloading mysql communit reop
-wget https://repo.mysql.com/mysql80-community-release-el8-1.noarch.rpm
+wget https://repo.mysql.com/https://repo.mysql.com/mysql84-community-release-el9-2.noarch.rpm
 echo "mysql repo downloading complete."
 
 #insalling mysql8 local repo
-yum localinstall -y mysql80-community-release-el8-1.noarch.rpm
+yum localinstall -y mysql84-community-release-el9-2.noarch.rpm
 echo "mysql repo local installation complete."
 
 #Verify MySQL Yum repository has been added successfully.
@@ -31,3 +31,18 @@ systemctl start mysqld
 #Get enable database services
 echo "Getting enable mysql service................................................................................................."
 systemctl enable mysqld
+
+#configure mysql initial configuration
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "+++++++++++++++Mysql initial configuration going to start.Input desire settings+++++++++++++++"
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+mysql_secure_installation
+
+#enable selinux permission
+semanage port -a -t mysqld_port_t -p tcp 3306
+
+#Restarting system
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "+++++++++++++++All Configurations done and System going to reboot..............+++++++++++++++"
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+reboot
