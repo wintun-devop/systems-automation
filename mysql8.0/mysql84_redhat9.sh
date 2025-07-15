52c069a9fd8258680981c7f6b9ffb7c7939afebe
Win Tun Hlaing <71956577+wintun-devop@users.noreply.github.com>
2025-07-15 13:35:00 +0630

    Update mysql84_redhat9.sh

--- mysql8.0/mysql84_redhat9.sh
+++ mysql8.0/mysql84_redhat9.sh
@@ -1,1 +1,33 @@
+#!/bin/bash
 
+#Win Tun Hlaing(https://www.youtube.com/channel/UCz9ebjc-_3t3p49gGpwyAKA/videos)
+
+#Necessary Update for system packages
+echo "Updating the system packages ......................................................................................................."
+dnf update -y
+echo "Packages update is done!!!!!!!!!!"
+
+#Installing necessary system packages before mysql package insalling
+dnf install -y gcc net-tools curl wget unzip
+echo "Necessary software packages installation done!"
+
+#Downloading mysql communit reop
+wget https://repo.mysql.com/mysql80-community-release-el8-1.noarch.rpm
+echo "mysql repo downloading complete."
+
+#insalling mysql8 local repo
+yum localinstall -y mysql80-community-release-el8-1.noarch.rpm
+echo "mysql repo local installation complete."
+
+#Verify MySQL Yum repository has been added successfully.
+dnf repolist enabled | grep "mysql.*-community.*"
+
+#Installing mysql server package
+dnf install -y mysql-server
+
+#Get start database services
+systemctl start mysqld
+
+#Get enable database services
+echo "Getting enable mysql service................................................................................................."
+systemctl enable mysqld
