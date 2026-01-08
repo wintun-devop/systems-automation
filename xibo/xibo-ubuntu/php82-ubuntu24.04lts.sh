1b1be02df8b6dbec0be4b8bca36ed583683fc9c2
Win Tun Hlaing <71956577+wintun-devop@users.noreply.github.com>
2026-01-08 10:43:18 +0630

    Update php82-ubuntu24.04lts.sh

--- xibo/xibo-ubuntu/php82-ubuntu24.04lts.sh
+++ xibo/xibo-ubuntu/php82-ubuntu24.04lts.sh
@@ -7,7 +7,7 @@ apt update -y
 apt upgrade -y
 
 #apache and necessary modules
-apt install -y wget curl make unzip network-manager gcc net-tools lsb-release ca-certificates apt-transport-https gnupg software-properties-common
+apt install -y wget curl make unzip network-manager gcc gnupg net-tools lsb-release ca-certificates apt-transport-https gnupg software-properties-common
 
 # install
 apt install -y apache2
@@ -22,7 +22,7 @@ systemctl enable --now apache2
 add-apt-repository ppa:ondrej/php -y
 apt update -y
 
-apt install -y php8.2 php8.2-cli php8.2-fpm php8.2-gd php8.2-xml php8.2-mbstring php8.2-zip php8.2-gettext php8.2-soap php8.2-curl php8.2-mysql php8.2-zmq libapache2-mod-php8.2
+apt install -y php8.2 php8.2-cli php8.2-fpm php8.2-gd php8.2-gnupg php8.2-xml php8.2-mbstring php8.2-zip php8.2-gettext php8.2-soap php8.2-curl php8.2-mysql php8.2-zmq libapache2-mod-php8.2
 
 #expose the php packages information
 echo "<?php phpinfo() ?>" >> /var/www/html/info.php
