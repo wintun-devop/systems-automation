1559e14a29bedec374510e5fd07a2c770d786a5d
Win Tun Hlaing <71956577+wintun-devop@users.noreply.github.com>
2026-05-15 10:54:23 +0630

    Install PHP 8.3 and necessary extensions
    Added installation of PHP 8.3 and related extensions.

--- wordpress/php83-ubuntu2604lts.sh
+++ wordpress/php83-ubuntu2604lts.sh
@@ -17,3 +17,16 @@ systemctl start apache2
 
 #get enable the apache server to run alway on starup
 systemctl enable --now apache2
+
+apt install -y php8.3 php8.3-cli php8.3-fpm libapache2-mod-php8.3
+
+apt install -y php8.3-gd php8.3-xml php8.3-mbstring php8.3-zip php8.3-soap php8.3-curl php8.3-mysql php8.3-intl php8.3-bcmath php8.3-gettext
+
+#expose the php packages information
+echo "<?php phpinfo() ?>" >> /var/www/html/info.php
+
+systemctl restart php8.3-fpm
+
+systemctl restart apache2
+
+php -m | grep -E 'phar|json|gd|dom|PDO|zip|gettext|soap|curl|iconv|ctype|fileinfo|xml|simplexml|mbstring|zlib|zmq'
